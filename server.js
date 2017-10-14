d53b7ce2caba75a74599063df3acf3076df27672
chrawrles <chrawrles@gmail.com>
2017-10-14 03:26:29 -0700

    ocnfig

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ require("./routes/html-routes.js")(app);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
-  server.listen(PORT, function() {
+  app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
